44d4195d08c72a9269158778dee12a57cbae9d28
RamFarid <workprojects22@gmail.com>
2023-10-25 06:47:14 +0300

    solve most pp

--- routes/member.js
+++ routes/member.js
@@ -2,6 +2,7 @@ const express = require('express')
 const memberRouter = express.Router()
 const Member = require('../models/Member')
 const responseMessage = require('../utils/responseMessage')
+const Quiz = require('../models/Quiz')
 
 memberRouter.put('/:id', async (res, req) => {
   try {
@@ -87,12 +88,56 @@ memberRouter.get('/search', async (req, res) => {
           _id: 1,
           name: 1,
           church: 1,
+          quizzesDone: 1,
+          createdAt: 1,
         },
       },
     ])
     res.json(responseMessage('Done', true, { data: members }))
   } catch (error) {
     res.status(200).json(responseMessage(error.message, false, { ...error }))
+  }
+})
+
+memberRouter.get('/waiting-members', async (req, res) => {
+  try {
+    const quizzes = await Quiz.aggregate([
+      {
+        $sort: {
+          title: -1,
+        },
+      },
+      {
+        $lookup: {
+          from: 'members',
+          let: { quizID: '$_id' },
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $not: {
+                    $in: ['$$quizID', '$quizzesDone'],
+                  },
+                },
+              },
+            },
+          ],
+          as: 'members',
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+          title: 1,
+          active: 1,
+          notes: 1,
+          members: 1,
+        },
+      },
+    ])
+    res.json(responseMessage('الاعضاء جم', true, { data: quizzes }))
+  } catch (error) {
+    res.json(responseMessage(error.message, false))
   }
 })
 
